6b21a82e57259a6136f8cbd4896c412381bc60a9
yuviip <yuvii.p@gmail.com>
2019-09-03 15:09:40 +0300

    adding Attributes to Soldier and CompanyCommander

--- Soldier.py
+++ Soldier.py
@@ -1,9 +1,27 @@
 import socket
 import logging
 
-
 # Initialize the Logger
 logging.basicConfig(filename = 'Log.log', level = logging.DEBUG, format = '%(asctime)s : %(levelname)s : Soldier : %(message)s')
+
+class Soldier():
+
+    # Attributes
+    ID = 1
+    companyNumber = 0
+    x = 0
+    y = 0
+    ammo = 0
+    HP = 0
+
+    def __init__(self, companyNumber, location, ammo):
+        self.ID = Soldier.ID
+        Soldier.ID += 1
+        self.companyNumber = companyNumber
+        self.x = location[0]
+        self.y = location[1]
+        self.ammo = ammo
+        self.HP = 100
 
 # getSock
 def getSock():
