cd982b476861122a286ed5d05a25c02667544a1b
Yuval Parness <yuvii.p@gmail.com>
2019-09-22 13:11:19 +0300

    update udp project - added FieldObjects and BTW - updated the names of variables and functions to lower case

--- CompanyCommanderUDP.py
+++ CompanyCommanderUDP.py
@@ -6,8 +6,8 @@ import Utility
 logging.basicConfig(filename = 'Log.log', level = logging.DEBUG, format = '%(asctime)s : %(levelname)s : CC : %(message)s')
 
 
-def handleMessage(rec_msg, rec_address):
-    case = Utility.switchCase(rec_msg)
+def handle_message(rec_msg, rec_address):
+    case = Utility.switch_case(rec_msg)
 
     if case == 1:
 
@@ -15,7 +15,7 @@ def handleMessage(rec_msg, rec_address):
         print('Received message from Soldier {} : {}'.format(rec_address, rec_msg))
         logging.debug("Received message from Soldier {} : {}".format(rec_address, rec_msg))
 
-        sock.sendto(rec_msg.encode(), Utility.getSoldierAddress())
+        sock.sendto(rec_msg.encode(), Utility.get_soldier_address())
 
     elif case == 2:
 
@@ -27,24 +27,24 @@ def handleMessage(rec_msg, rec_address):
         rec_msg = rec_msg[:-1]
 
         logging.debug("Received message from BC {} : {}".format(rec_address, rec_msg))
-        sock.sendto(rec_msg.encode(), Utility.getSoldierAddress())
+        sock.sendto(rec_msg.encode(), Utility.get_soldier_address())
 
     else:           # case = 0
         logging.ERROR("An invalid message has reached: \'{}\'".format(rec_msg))
 
 # *Main*
 # Initialize Server Address
-CCAddress = Utility.initCCAddress()
+cc_address = Utility.init_cc_address()
 
-if CCAddress == 0:   # 3 CompanyCommanders is open
+if cc_address == 0:   # 3 CompanyCommanders is open
     print("There are 3 Company Commanders already open in the system")
     quit()
 
-sock = Utility.getSock()
+sock = Utility.get_sock()
 
 
 # Bind the socket with the address
-sock.bind(CCAddress)
+sock.bind(cc_address)
 
 print('Listening')
 logging.debug('Listening')
@@ -52,11 +52,11 @@ logging.debug('Listening')
 while True:
 
     # set max size of message
-    recMsg, recAddress = sock.recvfrom(65527)
+    rec_msg, rec_address = sock.recvfrom(65527)
 
     # decoding the message to String
-    recMsg = recMsg.decode('utf-8')
+    rec_msg = rec_msg.decode('utf-8')
 
-    handleMessage(recMsg, recAddress)
+    handle_message(rec_msg, rec_address)
 
 
